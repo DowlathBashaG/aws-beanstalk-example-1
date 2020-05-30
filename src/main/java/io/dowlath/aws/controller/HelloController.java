de2ec2b4fc75e91064be3800ff4575e8c2c4a5e2
Arsh ADMIN Ariz <dowlathbasha@gmail.com>
2020-05-31 01:22:19 +0530

    added GetMapping

--- src/main/java/io/dowlath/aws/controller/HelloController.java
+++ src/main/java/io/dowlath/aws/controller/HelloController.java
@@ -1,5 +1,6 @@
 package io.dowlath.aws.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloController {
 
+    @GetMapping
     public String hello(){
         return "Hello Dowlath Basha G welcomes you....!!!";
     }
