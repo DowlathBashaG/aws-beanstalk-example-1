b0dd9029ea220d91fd668cd37de9ca1fc523e44c
Arsh ADMIN Ariz <dowlathbasha@gmail.com>
2020-05-30 23:59:21 +0530

    AWS Beanstalk example initial commit

---
+++ src/main/java/io/dowlath/aws/controller/HelloController.java
@@ -0,0 +1,17 @@
+package io.dowlath.aws.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @Author Dowlath
+ * @create 5/30/2020 11:44 PM
+ */
+@RequestMapping("/hello")
+@RestController
+public class HelloController {
+
+    public String hello(){
+        return "Hello Dowlath Basha G welcomes you....!!!";
+    }
+}
---
+++ src/test/java/io/dowlath/aws/AwsElasticbeanstalkApplicationTests.java
@@ -0,0 +1,13 @@
+package io.dowlath.aws;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwsElasticbeanstalkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
